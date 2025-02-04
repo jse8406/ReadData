c5f5a0807161f9678001a56c14ff0ac1f5ac7488
jse <73538772+jse8406@users.noreply.github.com>
2025-02-04 15:50:48 +0900

    UPDATE css

--- average.py
+++ average.py
@@ -4,34 +4,38 @@ import pandas as pd
 # 데이터베이스 경로
 db_path = 'db/priceDB.db'
 
-# 가져올 연도와 월
-year = '2024'
-month = '10'
+# 가져올 연도
+year = '2022'
 
-# 날짜 구간 설정
+# 날짜 구간 설정 (1~10일, 11~20일, 21~31일)
 date_ranges = [("01", "10"), ("11", "20"), ("21", "31")]
 
 # SQLite 연결
 conn = sqlite3.connect(db_path)
 
-for start, end in date_ranges:
-    query = f"""
-    SELECT date, predict_price, base_price
-    FROM price_set
-    WHERE date BETWEEN '{year}.{month}.{start}' AND '{year}.{month}.{end}'
-    ORDER BY date ASC;
-    """
-    df = pd.read_sql(query, conn)
+# 1월부터 12월까지 반복
+total = []
+for month in range(1, 13):
+    month_str = f"{month:02d}"  # 1 → '01', 2 → '02', ..., 10 → '10'
+    value = []
+    for start, end in date_ranges:
+        query = f"""
+        SELECT date, predict_price, base_price
+        FROM price_set
+        WHERE date BETWEEN '{year}.{month_str}.{start}' AND '{year}.{month_str}.{end}'
+        ORDER BY date ASC;
+        """
+        df = pd.read_sql(query, conn)
 
-    # 데이터가 있는 경우만 비율 계산 및 출력
-    if not df.empty:
-        df["ratio"] = df["predict_price"] / df["base_price"]
-        ratio_mean = round(df["ratio"].mean(), 3)
-        print(f"2024년 {month}월 {start}일 ~ {end}일 평균 비율:", ratio_mean)
-    else:
-        print(f"2024년 {month}월 {start}일 ~ {end}일 데이터가 없습니다.")
+        # 데이터가 있는 경우만 비율 계산 및 출력
+        if not df.empty:
+            df["ratio"] = df["predict_price"] / df["base_price"] * 100
+            ratio_mean = round(df["ratio"].mean(), 1)
+            value.append(ratio_mean)
+        else:
+            print(f"{year}년 {month_str}월 {start}일 ~ {end}일 데이터가 없습니다.")
+    total.append(value)
 
+print(total)
 # 연결 종료
 conn.close()
-
-
